8da7eff496020addf22b8742d964f71768feb7d4
Joseph W Donahue <joseph@sharperhacksllc.com>
2025-12-07 17:48:21 -0800

    NB: Updated dependencies, tweak the slnx file for dotnet tool compat.
      modified:   Constraints.UnitTests/AssemblyInfo.cs
      modified:   Constraints.UnitTests/Constraints.UnitTests.csproj
      modified:   Constraints.slnx

--- Constraints.UnitTests/AssemblyInfo.cs
+++ Constraints.UnitTests/AssemblyInfo.cs
@@ -1,19 +1,6 @@
 using System.Runtime.InteropServices;
 
-// In SDK-style projects such as this one, several assembly attributes that were historically
-// defined in this file are now automatically added during build and populated with
-// values defined in project properties. For details of which attributes are included
-// and how to customise this process see: https://aka.ms/assembly-info-properties
-
-
-// Setting ComVisible to false makes the types in this assembly not visible to COM
-// components.  If you need to access a type in this assembly from COM, set the ComVisible
-// attribute to true on that type.
-
+// Not going to support COM interop.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM.
-
-[assembly: Guid("1a1c9ee3-2e94-4a4d-9fa4-9db9b9e776cf")]
 
 [assembly: Parallelize(Scope = ExecutionScope.ClassLevel)]
